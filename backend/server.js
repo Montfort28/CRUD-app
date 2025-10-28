7c61a6c83fdf9c81557bb85c7db98db8a9b0bcc5
Montfort <mugishamontfort28@gmail.com>
2025-10-28 23:37:39 +0200

    connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+
 const app = express()
+
+app.use(cors())
+mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crud'
+})
 
 app.listen(3000, () => {
     console.log('listening')
